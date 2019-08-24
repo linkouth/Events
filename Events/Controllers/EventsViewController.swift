b4ca2d5e962ea6784aa0a3f7f5b5890edbe8ba83
Timur <dkatimullin@gmail.com>
2019-08-24 16:28:19 +0400

    Added EventsVC

---
+++ Events/Controllers/EventsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  EventsViewController.swift
+//  Events
+//
+//  Created by User on 24/08/2019.
+//  Copyright © 2019 Timur LLC. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewController: UIViewController {
+
+    @IBOutlet weak var eventsTableView: UITableView! {
+        didSet {
+            eventsTableView.dataSource = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        eventsTableView.register(UINib(nibName: "EventTableViewCell", bundle: nil), forCellReuseIdentifier: "EventTableViewCell")
+    }
+
+}
+
+extension EventsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: Data source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = eventsTableView.dequeueReusableCell(withIdentifier: "EventTableViewCell", for: indexPath) as? EventTableViewCell {
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+}
---
+++ Events/Views/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  Events
+//
+//  Created by User on 24/08/2019.
+//  Copyright © 2019 Timur LLC. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
